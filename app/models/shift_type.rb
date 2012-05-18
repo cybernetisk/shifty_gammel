c24cc010bc83efa7726d040a73387261a61a5a79
Mari Larsen <mari@1x-193-157-204-253.uio.no>
2012-05-18 18:59:32 +0200

    pushed ting

--- app/models/shift_type.rb
+++ app/models/shift_type.rb
@@ -1,5 +1,5 @@
 class ShiftType < ActiveRecord::Base
-  attr_accessible :description, :location, :title
+  attr_accessible :description, :location, :title, :ticket_value
   has_many :shifts, dependent: :destroy, inverse_of: :shift_type
   has_many :certifications, dependent: :destroy, inverse_of: :shift_type
 end
