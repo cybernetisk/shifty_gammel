79cb4c181c7bb2d096bc5a0d50f8a2e25d35eeb0
Sigurd Ljødal <sigurd@ljodal.no>
2012-05-25 19:21:36 +0200

    Add user to task

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -2,4 +2,5 @@ class Shift < ActiveRecord::Base
   attr_accessible :comment, :end, :leasing, :start, :training, :shift_type_id
   belongs_to :task
   belongs_to :shift_type
+  has_one :user, through: :task
 end
