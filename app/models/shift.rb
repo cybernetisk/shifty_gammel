8195ef7de989330406cda37f6d449784bf59466e
Øyvind Bakkeli <olivaq@gmail.com>
2012-06-22 22:49:49 +0200

    Added date and duration fields to shift model, and some unit tests

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -3,4 +3,35 @@ class Shift < ActiveRecord::Base
   belongs_to :task
   belongs_to :shift_type
   has_one :user, through: :task
+
+  def self.getAvailableShifts
+      shifts = Shift.joins(:task).where('tasks.user_id'=> nil) #remember to remove shifts that are finished
+      shifts
+  end
+
+  def self.start_date
+    return ""
+  end
+
+  def date
+    if self.start == nil
+      return ""
+    end
+    self.start.strftime("%Y-%m-%d")
+  end
+  
+  def date=(val)
+    self.start = DateTime.strptime(val, "%Y-%m-%d")
+  end
+
+  def duration
+    if self.start == nil || self.end == nil
+      return 0
+    end
+    self.end - self.start
+  end
+
+  def duration=(val)
+    self.end = val.hours.since self.start
+  end
 end
--- test/unit/shift_test.rb
+++ test/unit/shift_test.rb
@@ -4,4 +4,46 @@ class ShiftTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "date_get" do
+    s = Shift.new
+
+    s.start = DateTime.now
+
+    assert_equal s.date, DateTime.now.strftime("%Y-%m-%d")
+  end
+
+  test "date_set" do
+    s = Shift.new
+
+    t = DateTime.now
+    s.date = t.strftime("%Y-%m-%d")
+    
+    assert_not_nil s.start
+
+    assert_equal t.strftime("%Y-%m-%d"), s.start.strftime("%Y-%m-%d")
+  end
+
+  test "get_duration" do
+
+    s = Shift.new
+
+    t = DateTime.now
+
+    s.start = t
+
+    s.end = 1.hours.since t
+
+    assert_equal s.duration, 1.hours
+  end
+
+  test "set_duration" do
+    s = Shift.new
+
+    t = DateTime.now
+
+    s.start = t
+    s.duration = 1
+
+    assert_equal s.end, 1.hours.since(t)
+  end
 end
