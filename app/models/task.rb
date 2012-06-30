0b971acec56ef05b126535600039fb3a5579758c
Øyvind Bakkeli <olivaq@gmail.com>
2012-06-30 23:38:35 +0200

    Started working on Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ActiveRecord::Base
   attr_accessible :description, :title, :user_group_id, :user_id
 
   belongs_to :user
+  belongs_to :user_group
   has_one :shift
   has_one :ticket
 end
