54704f09ea195bdb0ad7ea3255d88e3e7d67f136
Øyvind Bakkeli <olivaq@gmail.com>
2012-07-19 16:30:25 +0200

    Updated models to skip task element

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,9 +1,10 @@
 class Ticket < ActiveRecord::Base
   attr_accessible :task_id, :used, :value, :comment
 
-  belongs_to :task
+  #belongs_to :task
   belongs_to :user
- 
+  belongs_to :issued_by, :foreign_key=>'issued_by_id', :class_name=>'User'
+  
   def self.getTicketHistoryForUser(user)
     tickets = Ticket.where(task_id: Task.select("id").where(user_id: user));
     tickets
