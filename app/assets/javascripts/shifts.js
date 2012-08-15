bc7cd8aa94f2ea2afc4c0e1eb77617ffd7ea3e2f
Øyvind Bakkeli <olivaq@gmail.com>
2012-08-15 21:50:30 +0200

    Working on calendar view, made it work but fridays look crowded.

--- app/assets/javascripts/shifts.js
+++ app/assets/javascripts/shifts.js
@@ -1,149 +1,159 @@
-$(document).ready(function() {
-    // Start point for the new shift
-    var start = 0;
-    var stop = 0;
+function getDate(date)
+{
+    return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
+}
 
-    // Variable to store the shift object, so we can change it later
-    var shift = null;
+function ShiftManager()
+{
+    this.shifts = {};
+    this.by_date = {};
 
-    // Variable to keep track of mouse down
-    var mouse_down = false; 
-    $(".day ul").mousedown(function(e) {
-        // Set the start point
-        start = e.pageY - $(this).offset().top;
-
-        start = Math.floor(start / 10) * 10;
-
-        // Min length is 15 minutes = 10 px
-        stop = start + 10;
-
-        // Track the mouse unntil it's moused up
-        mouse_down = true;
-
-        draw_shift(this);
-    }).mousemove(function(e) {
-        if (mouse_down) {
-            stop = e.pageY - $(this).offset().top;
-
-            stop = Math.ceil(stop / 10) * 10;
-
-            if (stop - start < 10) {
-                stop = start + 10;
-            }
-
-            draw_shift(this);
-        }
-    }).mouseup(function(e) {
-
-        // Calculate the end point
-        /*stop = e.pageY - $(this).offset().top;
-
-        stop = Math.ceil(stop / 10) * 10;
-
-        // Check that the endpoint is at least
-        // 15 minutes from the start point
-        if (stop - start < 10) {
-            stop = start + 10;
-        }
-
-        // Draw the shift
-        draw_shift(this); */
-
-        // toggle_popup();
-
-        mouse_down = false;
-
-        //shift = null;
-    });
-
-    function draw_shift(element) {
-        if (!shift) {
-            shift = $("<li class=\"event\"></li>");
-        }
-        shift.css("top", start + "px");
-        shift.css("height", (stop - start) + "px");
-        $(element).append(shift);
-    }
-
-    function getDate(dt)
+    this.updateColumns = function(shift, high)
     {
-        return dt.getFullYear().toString() + "_" + dt.getMonth().toString() + "_" + dt.getDate().toString();
-    }
-
-    function setXAxis(shift, date)
-    {
-        var s = new Date(date);
-        var dow = s.getDay();
-        shift.css('left', Math.round(dow/7*100)+"%");
-        shift.css('width', Math.round(1.0/7*100) + "%");
-    }
-
-    function ShiftManager()
-    {
-        this.shifts = Array();
-        this.addShift = function(shift)
+        if(shift.columns < high)
         {
-            shift.t_start = new Date(shift.start);
-            shift.t_end = new Date(shift.end);
-            this.shifts.push(shift);
-        }
-
-        this.collides = function(a,b)
-        {
-            if(a.t_start < b.t_start &&
-                 a.t_end > b.t_start)
-                 return true;
-            if(a.t_start < b.t_end &&
-                a.t_end > b.t_end)
-                 return true;
-            
-        }
-        this.collisions = function(shift)
-        {
-            shift.t_start = new Date(shift.start);
-            shift.t_end = new Date(shift.end);
-
-            count = 0;
-            for(var i in this.shifts)
-            {
-
-
-            }
-                }
-        }
-    }
-
-    function render_shift(shift)
-    {
-        var start = getDate(new Date(shift.start));
-        var stop = getDate(new Date(shift.end));
-        var round = Math.round;
-        if(start != stop)
-        {
-            var s = ich.shift(shift);
-            s.css('top', round(new Date(shift.start).getHours() / 24 * 100) + "%");
-            s.css('bottom', "0%");
-            setXAxis(s, shift.start);
-            $("#calendar").append(s);
-
-            var s = ich.shift(shift);
-            s.css('top', "0%");
-            s.css('bottom', round(100 - new Date(shift.end).getHours() / 24 * 100) + "%");
-            setXAxis(s, shift.stop);
-            $("#calendar").append(s)
+            shift.columns = high;
+            this.setIndex(shift);
+            for(var i in shift.collisions)
+                this.updateColumns(shift.collisions[i], high);
+            return high;
         }
         else
         {
-            var s = ich.shift(shift);
-            s.css('top', round(new Date(shift.start).getHours() / 24 * 100) + "%");
-            s.css('bottom', round(100 - new Date(shift.end).getHours() / 24 * 100) + "%");
-            setXAxis(s, shift.start);
-            $("#calendar").append(s);
+            return shift.columns;
         }
     }
 
-    $.get("/shifts/calendar.json", function(data){
-        for(var i in data)
-            render_shift(data[i]);
-    })
-});
+    this.setIndex = function(shift)
+    {
+        for(var i = 0; i < shift.columns; i++)
+        {
+            var free = true;
+            for(var j in shift.collisions)
+                if(shift.collisions[j].index != undefined &&
+                    shift.collisions[j].index == i)
+                        free = false;
+            if(free)
+            {
+                shift.index = i;
+                return;
+            }
+        }
+
+    }
+    this.addShift = function(shift)
+    {
+        shift.t_start = new Date(shift.start);
+        shift.t_end = new Date(shift.end);
+        
+        if(!(shift.id in this.shifts))
+        {
+
+            this.shifts[shift.id] = shift;
+            
+            //this.render_shift(shift);
+
+            shift.collisions = this.collisions(shift);
+
+            shift.columns = 0;
+            this.updateColumns(shift, shift.collisions.length);
+
+            this.setIndex(shift);
+        }
+
+        
+//        if(!(shift.id in this.shifts) || shift != this.shifts[shift.id])
+//        {
+//
+//            this.shifts[shift.id] = shift;
+//            this.render_shift(shift)
+//        }
+
+
+    }
+
+    this.collides = function(a,b)
+    {
+        if(a.t_start <= b.t_start &&
+             a.t_end > b.t_start)
+             return true;
+
+        if(a.t_start <= b.t_end &&
+            a.t_end > b.t_end)
+             return true;
+
+        if(a.t_start >= b.t_start &&
+               a.t_end <= b.t_end)
+               return true;
+           
+        return false;
+    }
+
+    this.fix_columns = function(shift)
+    {
+        shift.collisions = this.collisions(shift);
+        
+    }
+
+    this.collisions = function(shift)
+    {
+        var collides = [];
+        for(var i in this.shifts)
+        {
+            if(this.collides(this.shifts[i], shift))
+                collides.push(this.shifts[i]);
+        }
+
+        return collides;
+    }
+
+}
+
+function render_shift(shift)
+{
+    var start = getDate(shift.t_start);
+    var stop = getDate(shift.t_end);
+    
+    var round = Math.round;
+    $("shift_" + shift.id).remove()
+    shift.day = shift.t_start.getDay();
+    if(start != stop)
+    {
+        var s = ich.shift(shift);
+        s.css('top', round(shift.t_start.getHours() / 24 * 100) + "%");
+        s.css('bottom', "0%");
+        s.addClass("column_" + shift.index + "of" + shift.columns);
+        setXAxis(shift, s, shift.t_start);
+        s.addClass("shift_" + shift.id);
+        $("#calendar").append(s);
+
+        var s = ich.shift(shift);
+        s.css('top', "0%");
+        s.css('bottom', round(100 - shift.t_end.getHours() / 24 * 100) + "%");
+        s.addClass("column_" + shift.index + "of" + shift.columns);
+        setXAxis(shift, s, shift.t_end);
+        s.addClass("shift_" + shift.id);
+        $("#calendar").append(s)
+    }
+    else
+    {
+        var s = ich.shift(shift);
+        s.css('top', round(shift.t_start.getHours() / 24 * 100) + "%");
+        s.css('bottom', round(100 - shift.t_end.getHours() / 24 * 100) + "%");
+        s.addClass("shift_" + shift.id);
+        s.addClass("column_" + shift.index + "of" + shift.columns);
+        setXAxis(shift, s, shift.t_start);
+        $("#calendar").append(s);
+    }
+}
+
+function setXAxis(e, s, d)
+{
+    var day = d.getDay() * 200;
+
+    var p = parseFloat(e.index) / e.columns;
+
+    var per = Math.round(p * 200 + day);
+    s.css('width', Math.round(200 / e.columns) + "px");
+    s.css("left", per + "px");
+}
