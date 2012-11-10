2b267517255763b2a594d14b64acb67ab2381df9
Henrik Steen <henrist@henrist.net>
2012-11-10 15:56:08 +0100

    Moved javascript from html-view til js-file for calendar-view.

--- app/assets/javascripts/shifts.js
+++ app/assets/javascripts/shifts.js
@@ -319,3 +319,260 @@ function FilterList(con, label, name, options, ds, multiple)
 
     con.append(self.element)
 }
+
+
+$(document).ready(function() {
+    if ($("#view_calendar").length == 0) return;
+    
+    // buttons for different views
+    $("#calendar_view").click(function(){
+        datasource.setOutput(new CalendarView($("#calendar"), datasource.start, datasource.stop));
+    });
+    $("#list_view").click(function(){
+        datasource.setOutput(new ListView($("#calendar"), datasource.start, datasource.stop));
+    });
+
+    $("#template").click(function(){
+        var d = $("<div>");
+
+        d.css({top:$(this).offset().top, left:$(this).offset().left, position:'absolute', width:200, background:'white', border:'1px solid black', padding:20});
+        $.get('/templates.json', function(data)
+        {
+            for(var i = 0; i < data.length; i++)
+            {
+                var c = data[i];
+                var m = $("<a href=\"#\"></a>");
+                m.html(c.title);
+                m.css('display','block');
+                d.append(m);
+            }
+            $(document.body).append(d);
+
+        });
+
+        d.mouseout(function(event){
+            if(d[0] != event.toElement && d.has(event.toElement).length == 0)
+                $(this).remove()
+        });
+    });
+
+
+    // 
+    (function($){
+        $.fn.disableSelection = function() {
+            return this
+                     .attr('unselectable', 'on')
+                     .css('user-select', 'none')
+                     .on('selectstart', false);
+        };
+    })(jQuery);
+
+    // create shift
+    $("#create_shift").click(function(){
+        var d = $("<div>");
+
+        d.css({top:$(this).offset().top, left:$(this).offset().left, position:'absolute', width:200, background:'white', border:'1px solid black', padding:20});
+
+        $.get('/shift_types.json', function(data)
+        {
+
+            function addShiftType(c)
+            {
+                var shift_type_id = c.id;
+                var m = $("<a href=\"#\"></a>");
+                m.html(data[i].title);
+                m.css('display','block');
+
+                m.click(function(){createShiftInCalendar(c)});
+                d.append(m);
+            }
+
+            for(var i = 0; i < data.length; i++)
+            {
+                addShiftType(data[i]);
+            }
+
+            $(document.body).append(d);
+        });
+
+
+        d.mouseout(function(event){
+            if(d[0] != event.toElement && d.has(event.toElement).length == 0)
+                $(this).remove()
+        });
+    });
+
+
+
+
+    $(".pickType").live('click', function(event)
+    {
+        var x = $(".pickType.picked")
+        var id = x.attr('shift_type');
+        cv.filter({'shift_type':id});
+    });
+
+    function resized(event, ui)
+    {
+        var e = $(this);
+        
+        var s = e.data('shift');
+
+        if(ui.position.top != ui.originalPosition.top)
+        {
+            var rx = ui.position.left / $("#calendar").width() * 100;
+            var ry = ui.position.top / $("#calendar").height() * 100;
+            var t = cv.offsetToTime(rx, ry);
+        
+            s.start = t;
+        }
+        else
+        {
+            var rx = ui.position.left / $("#calendar").width() * 100;
+            var ry = (ui.position.top + ui.size.height) / $("#calendar").height() * 100;
+            var t = cv.offsetToTime(rx, ry);
+            s.end = t;
+        }
+
+        cv.refresh();
+        makeEditable()
+        changed(s)
+    }
+
+    function moved(event, ui)
+    {
+        var e = $(this);
+        var s = e.data('shift');
+
+        var t = cv.offsetToTime(ui.position.left    / $("#calendar").width() * 100, ui.position.top / $("#calendar").height() * 100);
+        
+        var diff = t.valueOf() - s.start.valueOf();
+
+
+        s.start = new Date(s.start.valueOf() + diff);
+        s.end = new Date(s.end.valueOf() + diff);
+
+        cv.refresh();
+        makeEditable()
+        changed(s);
+    }
+
+    function changed(shift)
+    {
+        var start = getISODateTime(shift.start);
+        var end = getISODateTime(shift.end);
+        $.post('/shifts/' + shift.id +".json" , {'_method':'put', 'shift[start]':start, 'shift[end]':end}, function(e){
+
+        });
+    }
+
+    function makeEditable()
+    {
+        
+        $(".shift").not(".ui-draggable").draggable(
+                {
+                grid: [100/7, 1000/24/4],
+                stop: moved
+                });
+
+        $(".shift").not(".ui-resizable").resizable(
+            {
+                handles: "n, s",
+                grid: [10, 1000/24/4],
+                stop: resized
+             });
+
+    }
+
+    var cv = false;
+    var datasource = undefined;
+    $(function(){
+        var s = new Date();
+        s.setHours(0);
+
+        s = new Date().moveToDayOfWeek(1, -1);
+        s.setHours(0)
+
+        var e = new Date(s.getTime()).add(7).days();
+
+        datasource = new DataSource(s, e);
+
+        if(window.innerWidth <= 800)
+        {
+            datasource.output = new ListView($("#calendar"), s, e);
+        }
+        else
+            datasource.output = new CalendarView($("#calendar"), s, e);
+
+        new WeekPicker($("#picker"), s, e, datasource);
+        datasource.fetch();
+
+        makeEditable();
+        
+        new FilterList($("#filters"), "Shift types", "shift_type", shifttypes, datasource, true);
+        new FilterList($("#filters"), "Taken", "taken", {0:'Taken', 1:'Available'}, datasource);
+        
+        if (user.id) {
+            var list = {};
+            list[user.id] = user.username;
+            new FilterList($("#filters"), "User", "user_id", list, datasource);
+        }
+
+        $("#show_filter").click(function(e)
+        {
+                $("#filters").toggleClass('hidden');
+            if($("#filters").hasClass("hidden"))
+                $(this).text("+");
+            else
+                $(this).text("-");
+        });
+
+        $("#filter_search").keyup(function(e)
+        {
+            var value = this.value.trim();
+            value = value.split(/\s+/);
+            var filter = Array()
+            var username = Array();
+
+            for(var i = 0; i < value.length; i++) 
+            {
+                if(Date.parse(value[i]))
+                {
+                    cv.setTime(Date.parse(value[i]));
+                }
+                else if(value[i] != "")
+                    filter.push( value[i] + ".*");
+            }
+
+            $(".selected").removeClass("selected");
+            
+            if(filter.length > 0)
+            {
+                filter = new RegExp("(^" + filter.join("|^") + ")", "i");
+
+                var e = function(){
+                    if(this.innerText.match(filter))
+                    {
+                        $(this).addClass("selected");
+                        }
+                }
+                $(".option").each(e);
+            }
+            $(".picker").each(function(){
+                $(this).data("FilterList").updateFilter();
+            });
+        });
+
+        $("#calendar").mousedown(function(e){
+            p = e.srcElement;
+            while(p != this && !$(p).hasClass("shift"))
+                p = p.parentNode;
+            if(p != this) return;
+            e.preventDefault();
+            return 1;
+        });
+
+        //window.setInterval(function(){cv.update();makeEditable()}, 5000);
+    });
+
+});
