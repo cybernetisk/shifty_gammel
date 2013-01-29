1478fb63a8c34d2b4cdd8dc54d57917fd18f484b
Øyvind Bakkeli <olivaq@gmail.com>
2013-01-29 22:52:43 +0100

    Rails raven seems to steal the exceptions from the web view, only use in production..

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,4 +1,3 @@
 Raven.configure do |config|
   config.dsn = 'http://8f04da239fcc468296c04b7f5939d118:d0a9f0dc15e548308b761c37c8136618@erleken.com:9000/2'
-  config.environments = %w[ development production ]
 end
