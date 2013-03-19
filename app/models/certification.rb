3818097ab2a6056a1fd29b2a74a99b99c22a6c69
Øyvind Bakkeli <olivaq@gmail.com>
2013-03-19 20:16:47 +0100

    Manager must be accessible

--- app/models/certification.rb
+++ app/models/certification.rb
@@ -1,5 +1,5 @@
 class Certification < ActiveRecord::Base
-  attr_accessible :shift_type, :user_group
+  attr_accessible :shift_type, :user_group, :manager
   belongs_to :shift_type
   belongs_to :user_group
 end
